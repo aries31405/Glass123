6b6b9ef13892b962034b1cfe48eda6f8f94c09b8
廖冠閔 <adg7485961@yahoo.com.tw>
2016-01-12 06:13:40 +0800

    glass map

---
+++ GlassLogin/app/src/main/java/com/example/glass123/glasslogin/GlassMap.java
@@ -0,0 +1,378 @@
+package com.example.glass123.glasslogin;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.glass123.glasslogin.CreativeGlass.CreateQuestion.GetServerMessage;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GlassMap extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    String[] all,titleId,x,y;
+    String msg;
+    private GoogleMap mMap;
+
+    // Google API用戶端物件
+    private GoogleApiClient googleApiClient;
+
+    // Location請求物件
+    private LocationRequest locationRequest;
+
+    Marker customMarker;
+
+    Bitmap bitmap;
+
+    Handler handler =  new Handler();
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glass_map);
+        // 建立Google API用戶端物件
+        configGoogleApiClient();
+
+        // 建立Location請求物件
+        configLocationRequest();
+
+        googleApiClient.connect();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+            // Use default InfoWindow frame
+            @Override
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+
+            // Defines the contents of the InfoWindow
+            @Override
+            public View getInfoContents(Marker marker) {
+
+                // 依指定layout檔，建立地標訊息視窗View物件
+                View infoWindow = getLayoutInflater().inflate(R.layout.map_infowindow, null);
+                // 顯示地標title
+                TextView title = ((TextView) infoWindow.findViewById(R.id.map_titleId_text));
+                title.setText(marker.getTitle());
+
+                TextView content = ((TextView) infoWindow.findViewById(R.id.map_content_text));
+                content.setText(String.valueOf(marker.getPosition()));
+
+                //ImageView  image = ((ImageView) infoWindow.findViewById(R.id.map_image));
+
+                //image.setImageBitmap(bitmap);
+
+                return infoWindow;
+
+            }
+        });
+
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                LatLng place = new LatLng(latLng.latitude, latLng.longitude);
+
+                moveMap(place);
+            }
+        });
+
+    }
+
+
+    //從網路加載圖片
+    public Bitmap getBitmapFromURL(String imageUrl){
+        try
+        {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(input);
+            return bitmap;
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // 移動地圖到參數指定的位置
+    private void moveMap(LatLng place) {
+        // 建立地圖攝影機的位置物件
+        CameraPosition cameraPosition =
+                new CameraPosition.Builder()
+                        .target(place)
+                        .zoom(19)
+                        .build();
+
+        // 使用動畫的效果移動地圖
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    // 建立Google API用戶端物件
+    private synchronized void configGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    // 建立Location請求物件
+    private void configLocationRequest() {
+        locationRequest = new LocationRequest();
+        // 設定讀取位置資訊的間隔時間為一秒（1000ms）
+        locationRequest.setInterval(1000);
+        // 設定讀取位置資訊最快的間隔時間為一秒（1000ms）
+        locationRequest.setFastestInterval(1000);
+        // 設定優先讀取高精確度的位置資訊（GPS）
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // 已經連線到Google Services
+        // 啟動位置更新服務
+        // 位置資訊更新的時候，應用程式會自動呼叫LocationListener.onLocationChanged
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, GlassMap.this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // Google Services連線中斷
+        // int參數是連線中斷的代號
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // Google Services連線失敗
+        // ConnectionResult參數是連線失敗的資訊
+        int errorCode = connectionResult.getErrorCode();
+
+        // 裝置沒有安裝Google Play服務
+        if (errorCode == ConnectionResult.SERVICE_MISSING) {
+            Toast.makeText(this, "裝置沒有安裝Google Play服務", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        /*new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bitmap = getBitmapFromURL("http://163.17.135.76/TTS/20160111153925/DSC_0029.jpg");
+            }
+
+        }).start();*/
+
+        // 位置改變
+        LatLng place = new LatLng(location.getLatitude(), location.getLongitude());
+
+        // 移動地圖到目前的位置
+        moveMap(place);
+
+        googleApiClient.disconnect();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                GetServerMessage g = new GetServerMessage();
+                msg = g.all("http://163.17.135.76/glass/map_question_search.php","UserId="+"20151211151346511431");
+                handler.post(split);
+            }
+
+        }).start();
+
+    }
+
+    //更新UI
+    final Runnable uiupdate = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            for (int i = 0;i < titleId.length;i++)
+            {
+                LatLng place = new LatLng(Double.parseDouble(x[i]), Double.parseDouble(y[i]));
+
+                setUpMap(place,titleId[i]);
+            }
+        }
+    };
+
+    final Runnable split = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            String[] all;
+            all = msg.split("&");
+            titleId = all[0].split("-");
+            x = all[1].split("-");
+            y = all[2].split("-");
+
+            handler.post(uiupdate);
+
+        }
+    };
+
+
+    private void setUpMap(final LatLng markerLatLng,String titleId) {
+
+        View marker = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.custom_marker_layout, null);
+        TextView numTxt = (TextView) marker.findViewById(R.id.num_txt);
+        numTxt.setText(titleId);
+
+        customMarker = mMap.addMarker(new MarkerOptions()
+                .position(markerLatLng)
+                .title(titleId)
+                .snippet("Description")
+                .icon(BitmapDescriptorFactory.fromBitmap(createDrawableFromView(this, marker))));
+
+        final View mapView = getSupportFragmentManager().findFragmentById(R.id.map).getView();
+        if (mapView.getViewTreeObserver().isAlive()) {
+            mapView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @SuppressLint("NewApi")
+                // We check which build version we are using.
+                @Override
+                public void onGlobalLayout() {
+                    LatLngBounds bounds = new LatLngBounds.Builder().include(markerLatLng).build();
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                        mapView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    } else {
+                        mapView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    }
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+                }
+            });
+        }
+    }
+
+    // Convert a view to bitmap
+    public static Bitmap createDrawableFromView(Context context, View view) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        view.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+
+        return bitmap;
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "GlassMap Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.glass123.glasslogin/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "GlassMap Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.glass123.glasslogin/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
